4b27f749184c3779e3c2d493494abe388ecc7dc0
Lycoris_nya <cat26022002@mail.ru>
2021-11-05 14:26:29 +0500

    Match and Game class added

--- TableTennisDomain/Match.cs
+++ TableTennisDomain/Match.cs
@@ -1,7 +1,49 @@
-﻿namespace TableTennisDomain
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TableTennisDomain
 {
     public class Match
     {
+        public string Id { get; private set; }
+
+        public DateTime Date;
+        public IReadOnlyList<Game> Games { get; private set; }
         
+        public IReadOnlyList<Player> FirstTeam { get; private set; }
+        public IReadOnlyList<Player> SecondTeam { get; private set; }
+
+        public IReadOnlyList<Player> Winners
+        {
+            get
+            {
+                return Status switch
+                {
+                    GameStatus.Draw => new List<Player>(),
+                    GameStatus.FirstTeamWon => FirstTeam,
+                    _ => SecondTeam
+                };
+            }
+        }
+
+        public GameStatus Status
+        {
+            get
+            {
+                return Games.Select(game => game.Status)
+                    .GroupBy(status => status)
+                    .OrderByDescending(g => g.Count()).First().Key;
+            }
+        }
+
+        public Match(string id, List<Player> firstTeam, List<Player> secondTeam, List<Game> games, DateTime date = default)
+        {
+            Id = id;
+            FirstTeam = firstTeam;
+            SecondTeam = secondTeam;
+            Games = games;
+            Date = date == default ? DateTime.Now : date;
+        }
     }
 }
